e5db9dcc6590427f3d7e8c09d7586cc95367a9df
Symone Lewis <hohenssi@gmail.com>
2019-08-09 20:48:42 -0400

    added learn more functions

--- starter_code/JS/index.js
+++ starter_code/JS/index.js
@@ -1,14 +1,26 @@
 function showReadMore() {
+    $("#show-this-on-click").slideDown();
     $("#show-this-on-click").show();
     $(".readmore").hide();
+    $(".readless").slideDown();
     $(".readless").show();
 }
 
 function hideReadMore() {
+    $("#show-this-on-click").slideUp();
     $("#show-this-on-click").hide();
     $(".readmore").show();
+    $(".readless").slideUp();
     $(".readless").hide();
+}
+
+function learnMore() {
+    $("#learnmoretext").slideDown();
+    $(".learnmore").hide();
 }
 
 $(".readmore").click(showReadMore);
 $(".readless").click(hideReadMore);
+$(".learnmore").click(learnMore);
+
+
