d36479b0112e99d45757e40825681e68cbada2ee
Symone Lewis <hohenssi@gmail.com>
2019-08-09 20:38:04 -0400

    read more and read less finished

--- starter_code/JS/index.js
+++ starter_code/JS/index.js
@@ -1,5 +1,14 @@
 function showReadMore() {
     $("#show-this-on-click").show();
+    $(".readmore").hide();
+    $(".readless").show();
+}
+
+function hideReadMore() {
+    $("#show-this-on-click").hide();
+    $(".readmore").show();
+    $(".readless").hide();
 }
 
 $(".readmore").click(showReadMore);
+$(".readless").click(hideReadMore);
