6d0dc5f56486162fe7be4f0073bcc407b168ca0d
hiralmatalia89 <hiralmatalia89@gmail.com>
2019-08-14 21:21:35 -0400

    Submission

--- starter_code/js/jqr.js
+++ starter_code/js/jqr.js
@@ -1,25 +1,28 @@
-$(document).ready();
-$('.read-more').click(function () {
+$(document).ready(onReady);
+function onReady(){
+
+$('.read-more').click(function() {
     $("#show-this-on-click").slideDown();
     $('.read-more').hide();
     $(".readless-hide").show();
 });
 
-$('.readless-hide').click(function () {
+$('.readless-hide').click(function() {
     $("#show-this-on-click").slideUp();
     $('.read-more').show();
     $(".readless-hide").hide();
 });
 
-$('.learnmore').click(function () {
+$('.learnmore').click(function() {
     $("#learnmoretext").slideDown();
     $('.learnmore').hide();
     $('.readLessHide').show();
 });
 
-$('readLessHide').click(function () {
+$('readLessHide').click(function() {
     $("#learnmoretext").slideUp();
     $('.readLessHide').hide();
     $('.learnmore').show();
 
 });  
+}
